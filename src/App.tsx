fcca0109829210c63bdab0259f74ddf5fb4e1a08
Jack Li <50118799+litDumpling@users.noreply.github.com>
2024-04-20 02:34:45 -0400

    Adds time counter since incident

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./styles.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const getMessage = () => {
+  const dayHeScrewedUp = new Date("04/12/2024");
+  const today = new Date();
+
+  // get total seconds between the times
+  let delta = Math.abs(today.getTime() - dayHeScrewedUp.getTime()) / 1000;
+
+  // calculate (and subtract) whole days
+  const days = Math.floor(delta / 86400);
+  delta -= days * 86400;
+
+  // calculate (and subtract) whole hours
+  const hours = Math.floor(delta / 3600) % 24;
+  delta -= hours * 3600;
+
+  // calculate (and subtract) whole minutes
+  const minutes = Math.floor(delta / 60) % 60;
+  delta -= minutes * 60;
+
+  // what's left is seconds
+  const seconds = Math.floor(delta % 60);
+
+  return `${days} Days ${hours} hours ${minutes} minutes and ${seconds} seconds`;
+};
+
+export default function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setMessage(getMessage());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="message">
+        <div className="header">{message}</div>
+        <div className="subHeader"> since Joe screwed up.</div>
+        <div className="warning">
+          Let this be a lesson to not click on suspicious links that asks for
+          your discord login.
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
